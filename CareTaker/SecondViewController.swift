b142fa10590d05619c77a961a97219f459d19fc0
Chris Fischer <chris.fischer36@gmail.com>
2017-04-08 19:48:23 -0400

    Initial Commit

---
+++ CareTaker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CareTaker
+//
+//  Created by Chris Fischer on 4/8/17.
+//  Copyright © 2017 Chris Fischer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
